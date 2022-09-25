5d9d71b734405b44411a5908a420eb5cbb5169e3
BobTheFarmer <colinmweis@gmail.com>
2022-09-24 23:53:56 -0700

    testing

--- bp_projects/projects.py
+++ bp_projects/projects.py
@@ -4,12 +4,10 @@ app_projects = Blueprint('projects', __name__,
                 url_prefix='/projects',
                 template_folder='templates/bp_projects/')
 
-# connects /kangaroos path to render kangaroos.html
 @app_projects.route('/portfolio/')
 def portfolio():
     return render_template("portfolio.html")
 
-# connects /kangaroos path to render kangaroos.html
 @app_projects.route('/purpose-roles/')
 def purposeAndRoles():
     return render_template("purposeAndRoles.html")
