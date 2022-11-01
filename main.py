1ca4a6fb04ce560c3cdcd54858ca12ffdb1b6213
Dolphin <ethan.zhao.us@gmail.com>
2022-11-01 15:21:13 -0700

    deploy api

--- main.py
+++ main.py
@@ -4,9 +4,22 @@ from flask import render_template  # import render_template from "public" flask 
 from __init__ import app  # Definitions initialization
 from api import app_api # Blueprint import api definition
 from bp_projects.projects import app_projects # Blueprint directory import projects definition
+from flask import jsonify # used for weather data
+import requests
 
 app.register_blueprint(app_api) # register api routes
 app.register_blueprint(app_projects) # register api routes
+
+def makeSummary():
+    url = "https://visual-crossing-weather.p.rapidapi.com/forecast"
+    querystring = {"aggregateHours":"24","location":"Washington,DC,USA","contentType":"json","unitGroup":"us","shortColumnNames":"0"}
+    headers = {
+    	"X-RapidAPI-Key": "eb0bbc6cc0msh085b254f1761bc2p154cf4jsne59a8dbdbaa0",
+	    "X-RapidAPI-Host": "visual-crossing-weather.p.rapidapi.com"
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    weatherData=response.text
+    return weatherData
 
 @app.errorhandler(404)  # catch for URL not found
 def page_not_found(e):
@@ -24,6 +37,11 @@ def stub():
 @app.route('/functionAndPurpose/')  # connects /functionAndPurpose/ URL to functionAndPurpose() function
 def functionAndPurpose():
     return render_template("functionAndPurpose.html")
+
+@app.route('/weatherData/')  # allows access to weather data 
+def weatherData():
+    weatherData = makeSummary()
+    return jsonify(weatherData)
 
 # this runs the application on the development server
 if __name__ == "__main__":
